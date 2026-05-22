d62ac6de34afd3e0f935d88b20b6a1a021b4966c
Maria Heloiza Amorim <1550160@sga.pucminas.br>
2026-05-21 21:26:07 -0300

    criação da página home.js

---
+++ assets/js/home.js
@@ -0,0 +1,23 @@
+import { dados_das_noticias } from "./bd.js";
+
+let vetor_das_noticias = [];
+const container_das_noticias = document.getElementById("noticias");
+const opcao_noticia = document.getElementById("opcao-noticia");
+
+opcao_noticia.addEventListener("click", (e) => {
+    alternarApresentacaoDasNoticias();
+});
+
+function alternarApresentacaoDasNoticias(){
+    if (vetor_das_noticias.length == 0){
+        carregarNoticias();
+        apresentarNoticias();
+    } else {
+        container_das_noticias.innerHTML = ""; 
+        vetor_das_noticias = [];
+    }
+}
+
+const carregarNoticias = async(){
+
+}
