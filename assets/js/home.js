9017b02044bac0106cdabf09d5cb261318610b3f
Maria Heloiza Amorim Xavier <1550160@sga.pucminas.br>
2026-05-21 22:25:54 -0300

    versão final: aula 21/05

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,20 +4,45 @@ let vetor_das_noticias = [];
 const container_das_noticias = document.getElementById("noticias");
 const opcao_noticia = document.getElementById("opcao-noticia");
 
-opcao_noticia.addEventListener("click", (e) => {
-    alternarApresentacaoDasNoticias();
-});
+if (opcao_noticia) {
+    opcao_noticia.addEventListener("click", (e) => {
+        alternarApresentacaoDasNoticias();
+    });
+}
 
-function alternarApresentacaoDasNoticias(){
-    if (vetor_das_noticias.length == 0){
+function alternarApresentacaoDasNoticias() {
+    if (vetor_das_noticias.length === 0) {
         carregarNoticias();
         apresentarNoticias();
     } else {
-        container_das_noticias.innerHTML = ""; 
+        container_das_noticias.innerHTML = "";
         vetor_das_noticias = [];
     }
 }
 
-const carregarNoticias = async(){
+const carregarNoticias = async () => {
+    vetor_das_noticias = dados_das_noticias;
+};
 
+function apresentarNoticias() {
+    vetor_das_noticias.forEach(noticia => {
+        const cartao = document.createElement("div");
+        cartao.className = "cartao";
+
+        cartao.innerHTML = `
+        <img src="${noticia.imagem}" alt= "${noticia.titulo}" class="imagem-do-cartao">
+        <section class="corpo-do-cartao">
+        <h1 class ="titulo-do-cartao">${noticia.titulo}</h1>
+        <p class="classificacao">
+        <span class="categoria-da-noticia">${noticia.categoria}</span>
+        <span class="data-da-noticia">${noticia.descricao}
+        </p>
+        <p class ="descricao-da-noticia">${noticia.descricao}</p>
+        <p class ="autor-da-noticia">${noticia.autor}</p>
+        </section>
+        <a class="botao-do-cartao" href="./detalhe.html?id=${noticia.id}">Leia mais</a>
+        `;
+
+        container_das_noticias.appendChild(cartao);
+    });
 }
